73fad1b00917023ee0a99e9253604d4b366a232c
Pasquale De Marinis <pasquale199876@hotmail.it>
2025-07-04 17:25:21 +0200

    scripts

--- scripts.sh
+++ scripts.sh
@@ -2,3 +2,15 @@
 
 python main.py generate -p parameters/data/pascal.yaml
 python main.py generate -p parameters/data/coco.yaml
+
+# COCO
+python main.py grid --parameters parameters/coco/iauc_dauc_dcama.yaml --parallel
+python main.py grid --parameters parameters/coco/iauc_dauc_dmtnet.yaml --parallel
+python main.py grid --parameters parameters/coco/cut_iauc.yaml --parallel
+python main.py grid --parameters parameters/coco/cut_iauc_miou.yaml --parallel
+
+# PASCAL
+python main.py grid --parameters parameters/pascal/iauc_dauc_dcama.yaml --parallel
+python main.py grid --parameters parameters/pascal/iauc_dauc_dmtnet.yaml --parallel
+python main.py grid --parameters parameters/pascal/cut_iauc.yaml --parallel
+python main.py grid --parameters parameters/pascal/cut_iauc_miou.yaml --parallel
