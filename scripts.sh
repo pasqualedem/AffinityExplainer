e6851d0326d9a07a2cbca69715f2d77a44f76f19
Pasquale De Marinis <pasquale199876@hotmail.it>
2025-07-11 11:01:26 +0200

    N1K1 scripts

--- scripts.sh
+++ scripts.sh
@@ -16,12 +16,14 @@ python main.py grid --parameters parameters/coco/iauc_dauc_dcama.yaml --parallel
 python main.py grid --parameters parameters/coco/iauc_dauc_dmtnet.yaml --parallel
 python main.py grid --parameters parameters/coco/cut_iauc.yaml --parallel
 python main.py grid --parameters parameters/coco/cut_iauc_miou.yaml --parallel
+python main.py grid --parameters parameters/coco/cut_iauc_miou_N1K1.yaml --parallel
 
 # PASCAL
 python main.py grid --parameters parameters/pascal/iauc_dauc_dcama.yaml --parallel
 python main.py grid --parameters parameters/pascal/iauc_dauc_dmtnet.yaml --parallel
 python main.py grid --parameters parameters/pascal/cut_iauc.yaml --parallel
 python main.py grid --parameters parameters/pascal/cut_iauc_miou.yaml --parallel
+python main.py grid --parameters parameters/pascal/cut_iauc_miou_N1K1.yaml --parallel
 
 # Cross Domain
 python main.py grid --parameters parameters/cross/iauc_dauc_deepglobe.yaml --parallel
@@ -35,6 +37,9 @@ python main.py grid --parameters parameters/cross/cut_iauc_isic.yaml --parallel
 python main.py grid --parameters parameters/cross/cut_iauc_miou_deepglobe.yaml --parallel
 python main.py grid --parameters parameters/cross/cut_iauc_miou_lung.yaml --parallel
 python main.py grid --parameters parameters/cross/cut_iauc_miou_isic.yaml --parallel
+python main.py grid --parameters parameters/cross/cut_iauc_miou_deepglobe_N1K1.yaml --parallel
+python main.py grid --parameters parameters/cross/cut_iauc_miou_lung_N1K1.yaml --parallel
+python main.py grid --parameters parameters/cross/cut_iauc_miou_isic_N1K1.yaml --parallel
 
 # Feature Ablation
 python main.py grid --parameters parameters/feature_ablation.yaml --parallel
