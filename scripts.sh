59cdd83b8fe3ab1a495bbc0db8c3bf88d26e54ee
pasqualedem <pasquale199876@hotmail.it>
2025-07-11 10:34:58 +0200

    N1K1

--- scripts.sh
+++ scripts.sh
@@ -5,6 +5,11 @@ python main.py generate -p parameters/data/coco.yaml
 python main.py generate -p parameters/data/isic.yaml
 python main.py generate -p parameters/data/deepglobe.yaml
 python main.py generate -p parameters/data/lung.yaml
+python main.py generate -p parameters/data/pascal_N1K1.yaml
+python main.py generate -p parameters/data/coco_N1K1.yaml
+python main.py generate -p parameters/data/isic_N1K1.yaml
+python main.py generate -p parameters/data/deepglobe_N1K1.yaml
+python main.py generate -p parameters/data/lung_N1K1.yaml
 
 # COCO
 python main.py grid --parameters parameters/coco/iauc_dauc_dcama.yaml --parallel
