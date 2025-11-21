d1f80a0f370411dd0c927b7cde21bf635e9cf76f
pasqualedem <pasquale199876@hotmail.it>
2025-11-21 19:52:53 +0100

    YAML refactor

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -1,18 +1,3 @@
-# Data Generation Script
-
-python main.py generate -p parameters/data/pascal.yaml
-python main.py generate -p parameters/data/coco.yaml
-python main.py generate -p parameters/data/isic.yaml
-python main.py generate -p parameters/data/deepglobe.yaml
-python main.py generate -p parameters/data/lung.yaml
-python main.py generate -p parameters/data/pascal_N1K1.yaml
-python main.py generate -p parameters/data/pascal_N1K1_100.yaml
-python main.py generate -p parameters/data/coco_N1K1.yaml
-python main.py generate -p parameters/data/coco_N1K2.yaml
-python main.py generate -p parameters/data/isic_N1K1.yaml
-python main.py generate -p parameters/data/deepglobe_N1K1.yaml
-python main.py generate -p parameters/data/lung_N1K1.yaml
-
 # COCO
 python main.py grid --parameters parameters/coco/iauc_dauc_dcama.yaml --parallel
 python main.py grid --parameters parameters/coco/iauc_dauc_dmtnet.yaml --parallel
